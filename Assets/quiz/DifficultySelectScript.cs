af6d9e5001924d255c98157b8f457b550ba9dd0e
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-20 18:16:06 +0800

    Fixed missing scripts for difficulty selection

--- Assets/quiz/DifficultySelectScript.cs
+++ Assets/quiz/DifficultySelectScript.cs
@@ -1,27 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DifficultySelectScript : MonoBehaviour
 {
-    static public string difficultySelectionText;
-    
-
-    public void selectDifficulty()
+    public void OnNoviceButtonClick()
     {
-        switch (this.gameObject.name)
-        {
-            case "NoviceButton":
-                Difficulty.difficulty = "Novice";           
-                break;
-            case "IntermediateButton":
-                Difficulty.difficulty = "Intermediate";
-                break;
-            case "ExpertButton":
-                Difficulty.difficulty = "Expert";
-                break;
-        }
+        ShowQuizUi("Novice");
+    }
+    
+    public void OnIntermediateButtonClick()
+    {
+        ShowQuizUi("Intermediate");
+    }
+    
+    public void OnExpertButtonClick()
+    {
+        ShowQuizUi("Expert");
+    }
+
+    void ShowQuizUi(string difficulty)
+    {
+        Difficulty.difficulty = difficulty;
         SceneManager.LoadScene("QuizScene");
     }
 }
