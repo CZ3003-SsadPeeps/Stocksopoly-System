382ec359f42b7a3530b96ba251ee52d43c4d6156
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-21 13:28:29 +0800

    Linked UI with code

---
+++ Assets/leaderboard/LeaderboardUi.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderboardUi : MonoBehaviour
+{
+    public ScrollRect scrollView;
+    public Transform scrollViewContent;
+    public Text textPrefab;
+
+    public void OnBackButtonClick()
+    {
+
+    }
+}
