ea4ecbd85bd7593fd9ac178475f7395da0bf32a5
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-22 21:57:46 +0800

    Set button to launch a specific stock in a separate UI

--- Assets/stock/StockUi.cs
+++ Assets/stock/StockUi.cs
@@ -1,10 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StockUi : MonoBehaviour
 {
     public StockListUi stockList;
+
+    void Start()
+    {
+        stockList.SetItemClickListener(pos => {
+            StockStore.SelectedStockPos = pos;
+            SceneManager.LoadScene("Tesla");
+        });
+    }
 
     public void OnBackButtonClick()
     {
