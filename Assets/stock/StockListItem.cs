73e0cd37d0c832212cdbbd045ca2816946477c43
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-22 21:04:40 +0800

    Created script for each stock list item

---
+++ Assets/stock/StockListItem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StockListItem : MonoBehaviour
+{
+    public Text stockNameText;
+    public Button tradeButton;
+
+    public void SetStockName(string name)
+    {
+        stockNameText.text = name;
+    }
+
+    public void SetClickListener(Button.ButtonClickedEvent clickEvent)
+    {
+        tradeButton.onClick = clickEvent;
+    }
+}
