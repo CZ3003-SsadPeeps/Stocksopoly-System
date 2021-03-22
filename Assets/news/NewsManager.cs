2647516f564d467e44bcf78ba6e0b16277073812
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-22 14:42:04 +0800

    Updated change stock price method with latest Stock changes

--- Assets/news/NewsManager.cs
+++ Assets/news/NewsManager.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class NewsManager : MonoBehaviour
-{   
+{
     public RectTransform NewsDisplayed;
     public string newscontent;
     public RectTransform BackGround;
     static readonly System.Random random = new System.Random();
     public int FlucRate;
     public string CompanyName;
-   
+
     void Awake()
     {
         // to get the randomized news obj from NewsStore and pass to DisplayNews
@@ -24,7 +24,7 @@ public class NewsManager : MonoBehaviour
 
 
         //displays the news using the function below
-        DisplayNews(news.CompanyName,news.Content);
+        DisplayNews(news.CompanyName, news.Content);
 
         //need to find out how to only call change stock when end of all 4 player turns
         // ChangeStock(CompanyName,FlucRate);
@@ -44,41 +44,20 @@ public class NewsManager : MonoBehaviour
         Newsdisplayed.GetComponent<Text>().text = CName + CContent;
     }
 
-
-
     // function to change the stock affected by fluc rate, while randomizing the chanage for other stocks
     public void ChangeStock(string Cname, int FlucRate)
     {
-        System.Random random = new System.Random();
-        for (int i=0; i < StockStore.stocks.Length; i++)
+        foreach (Stock stock in StockStore.stocks)
         {
-            Stock stock = StockStore.stocks[i];
-            int lastprice = stock.StockPriceHistory[9];
-            if(stock.Name == Cname)
-            {   
-
-                // save a new stock queue, dequeue and then enqueue the newest price
-                var StockQueue = new Queue<int>(stock.StockPriceHistory);
-                StockQueue.Dequeue();
-                StockQueue.Enqueue(lastprice * (FlucRate + 100) / 100);
-                var StockList = new List<int>(StockQueue);
-                stock.StockPriceHistory = StockList;
-
-            }
-
-            else
+            if (stock.Name == Cname)
             {
-                var StockQueue = new Queue<int>(stock.StockPriceHistory);
-                StockQueue.Dequeue();
-                StockQueue.Enqueue(random.Next(lastprice-10,lastprice+10));
-                var StockList = new List<int>(StockQueue);
-                stock.StockPriceHistory = StockList;
+                stock.ChangePrice(FlucRate);
+                continue;
             }
 
-
+            stock.ChangePrice(0);
         }
-
     }
 
-    
+
 }
