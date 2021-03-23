0458f27d6831cae708de59e59563f25e8ab8f4c3
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-23 14:21:33 +0800

    Compartmentalized logic for change stock amount

---
+++ Assets/stock/details/EditQuantity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EditQuantity : MonoBehaviour
+{
+    public Text QuantityText;
+    public Text StockPriceText;
+
+    public int Quantity { get; private set; };
+
+    Action<int> QuantityChangeListener;
+
+    public void OnQuantityChange(bool isIncrease)
+    {
+        if (isIncrease)
+        {
+            Quantity++;
+        }
+        else
+        {
+            Quantity--;
+        }
+
+        QuantityText.text = Quantity.ToString();
+        QuantityChangeListener.Invoke(Quantity);
+    }
+
+    public void SetQuantity(int quantity)
+    {
+        Quantity = quantity;
+        QuantityText.text = Quantity.ToString();
+    }
+
+    public void SetQuantityChangeListener(Action<int> changeListener)
+    {
+        QuantityChangeListener = changeListener;
+    }
+}
