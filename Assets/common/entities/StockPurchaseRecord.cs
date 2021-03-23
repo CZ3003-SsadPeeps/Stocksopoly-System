f89684603db72afd66b8a175846e5b8db374e2fa
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-23 20:12:14 +0800

    Added method to change stock quantity

--- Assets/common/entities/StockPurchaseRecord.cs
+++ Assets/common/entities/StockPurchaseRecord.cs
@@ -2,12 +2,22 @@
 {
     public string StockName { get; }
 
-    public int Quantity { get; } = 0;
+    public int Quantity { get; private set;  } = 0;
 
-    public int AverageStockPrice { get; } = 0;
+    public int AverageStockPrice { get; private set; } = 0;
 
     public StockPurchaseRecord(string stockName)
     {
         StockName = stockName;
+    }
+
+    public void AddQuantity(int quantity, int currentStockPrice)
+    {
+        if (quantity > 0)
+        {
+            AverageStockPrice = ((Quantity * AverageStockPrice) + (quantity * currentStockPrice)) / (Quantity + quantity);
+        }
+
+        Quantity += quantity;
     }
 }
