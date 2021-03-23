8cd287bce9a3dfbe02703adf70eeb1eb2c31e876
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-23 20:13:15 +0800

    Created manager class to perform logic for store details UI

---
+++ Assets/stock/details/StockDetailsManager.cs
@@ -0,0 +1,19 @@
+﻿class StockDetailsManager
+{
+    public Stock Stock { get; } = StockStore.SelectedStock;
+    public Player Player { get; } = GameStore.CurrentPlayer;
+    public StockPurchaseRecord PurchaseRecord { get; }
+    public int NewQuantity { get; set; }
+
+    internal StockDetailsManager()
+    {
+        // Cannot be initialized in field because Player & Stock attributes are not initialized yet
+        PurchaseRecord = StockStore.GetPurchaseRecord(Player.Name, Stock.Name);
+        NewQuantity = PurchaseRecord.Quantity;
+    }
+
+    internal void SaveQuantityChange()
+    {
+        PurchaseRecord.AddQuantity(NewQuantity - PurchaseRecord.Quantity, Stock.CurrentStockPrice);
+    }
+}
