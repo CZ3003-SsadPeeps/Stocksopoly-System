473cfee2b8268f533306132095b999ca6c43b893
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-23 14:27:38 +0800

    Linked stock details UI with code

---
+++ Assets/stock/details/StockDetailsUi.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StockDetailsUi : MonoBehaviour
+{
+    public Text stockNameText, buyAmountText, sellAmountText, playerDetails;
+    public Window_Graph graph;
+    public EditQuantity quantityEditor;
+    public Button buyButton, sellButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnBackButtonClick()
+    {
+
+    }
+}
