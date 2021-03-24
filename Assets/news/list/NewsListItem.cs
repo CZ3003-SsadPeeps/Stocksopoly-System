3f1e2fb1782eacdcb314932960919ba055837a33
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-03-24 17:04:53 +0800

    Set news list to display list of news

--- Assets/news/list/NewsListItem.cs
+++ Assets/news/list/NewsListItem.cs
@@ -4,4 +4,9 @@ using UnityEngine.UI;
 public class NewsListItem : MonoBehaviour
 {
     public Text newsText;
+
+    public void SetNews(News news)
+    {
+        newsText.text = $"{news.CompanyName} - {news.Content}";
+    }
 }
