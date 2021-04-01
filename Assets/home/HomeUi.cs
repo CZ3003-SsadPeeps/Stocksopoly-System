e87c8ab2d429f503d9a3553e7fe84f92c00c7d22
Khairuddin Bin Ali <khai0050@e.ntu.edu.sg>
2021-04-01 19:10:55 +0800

    Used proper English for class description

--- Assets/home/HomeUi.cs
+++ Assets/home/HomeUi.cs
@@ -2,7 +2,7 @@
 using UnityEngine.SceneManagement;
 
 /// <summary>
-/// The user interface that provides the option to start a game of Stocksopoly. This user interface is
+/// This is the user interface that provides the option to start a game of Stocksopoly. This user interface is
 /// the first one that runs when the game first launches.
 /// <br/><br/>
 /// Created by Khairuddin Bin Ali
