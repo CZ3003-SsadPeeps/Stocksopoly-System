449c7e4d15d8f9721c73f62eb3423e23e101edf6
chingtingng <62470831+chingtingng@users.noreply.github.com>
2021-04-05 14:19:22 +0800

    Update EventUi.cs

--- Assets/event/EventUi.cs
+++ Assets/event/EventUi.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// This script displays a random event and adds or deducts the player's credits according to the event shown.
+/// <br/><br/>
+/// Created by Lau Zhen Jie and Ng Ching Ting
+/// </summary>
 public class EventUi : MonoBehaviour
 {
+    /// <summary>
+    /// The text field where the event content and credits gained/lost will be displayed.
+    /// </summary>
     public Text contentText, creditText;
 
+    /// <summary>
+    /// Fetch the RectTransform from the GameObject.
+    /// </summary>
     public RectTransform PosTransform
     {
         get { return GetComponent<RectTransform>(); }
@@ -12,6 +23,11 @@ public class EventUi : MonoBehaviour
 
     readonly EventManager manager = new EventManager();
 
+    /// <summary>
+    /// Retrieve event from the manager and display a random event and adds or deducts the player's credits according to the event shown.
+    /// If the player receives credits, the text showing the player the amount of credits earned would be in green.
+    /// Else, the player's amount of credits to be deducted will be shown. 
+    /// </summary>
     public void LoadNewEvent()
     {
         EventRecord eventRecord = manager.GetEvent();
