fc1674fac57a837969caaa60af7b0971da569457
chingtingng <62470831+chingtingng@users.noreply.github.com>
2021-04-05 14:19:19 +0800

    Update EventPopupUi.cs

--- Assets/event/EventPopupUi.cs
+++ Assets/event/EventPopupUi.cs
@@ -2,8 +2,16 @@
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// This script retrieves a random event and adds or deducts the player's credits according to the event shown.
+/// <br/><br/>
+/// Created by Lau Zhen Jie and Ng Ching Ting
+/// </summary>
 public class EventPopupUi : MonoBehaviour
 {
+    /// <summary>
+    /// Retrieving of the random event content and amount of credits to add or deduct. 
+    /// </summary>
     public Text contentText, creditText;
 
     readonly EventManager manager = new EventManager();
@@ -25,6 +33,9 @@ public class EventPopupUi : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Close the event popup and continue playing with the game. 
+    /// </summary>
     public void OnConfirmButtonClick()
     {
         SceneManager.UnloadSceneAsync("EventPopup");
